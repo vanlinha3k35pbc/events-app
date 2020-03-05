1d87047e05d709fe6588839c4385ee63973e39c4
Nguyen Van Linh <linhnv36@vingroup.net>
2020-03-06 02:20:02 +0700

    Using vue components

--- src/router/index.js
+++ src/router/index.js
@@ -17,11 +17,18 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/event/:id',
+    name: 'eventSingle',
+    component: () => import('../views/EventSingle.vue')
   }
 ]
 
 const router = new VueRouter({
-  routes
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: routes
 })
 
 export default router

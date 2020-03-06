841b697b17b3090da381174727d50b8e1b479ace
Nguyen Van Linh <linhnv36@vingroup.net>
2020-03-06 09:24:53 +0700

    Add authentication

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,8 @@
           <h1 class="title">Welcome to the Animal Rescue League</h1>
           <h2 class="subtitle">Make sure you check out our upcoming events below</h2>
           <div class="button-block">
-            <button class="button is-xl is-dark">Sign Up to Browse Events</button>
+            <button v-if="!$auth.isAuthenticated" @click="login" class="button is-xl is-dark">Sign Up to Browse Events</button>
+            <h3 v-if="$auth.isAuthenticated" class="is-size-3 has-background-dark welcome">Welcome, {{ $auth.user.name }}!</h3>
           </div>
         </div>
       </div>
@@ -20,6 +21,12 @@ export default {
   name: "home",
   components: {
     EventsList
+  },
+  methods: {
+    // Log the user in
+    login() {
+      this.$auth.loginWithRedirect();
+    }
   }
 };
 </script>
